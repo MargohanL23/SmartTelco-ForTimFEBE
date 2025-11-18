a4ab56ffe40a444d26c4e3bc0a90709eceacbc33
varrel nuwi <varrel.nuwi@gmail.com>
2025-11-18 14:47:07 +0700

    tinggal styling

--- SmartTelco-FrontEnd/src/views/HasilView.vue
+++ SmartTelco-FrontEnd/src/views/HasilView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Halaman Hasil</h1>
+    <h1 class="text-2xl font-bold mb-4">Halaman Hasil Rekomendasi</h1>
     <p>Halaman hasil rekomendasi, nanti akan kita isi setelah flow utama selesai.</p>
   </div>
 </template>
