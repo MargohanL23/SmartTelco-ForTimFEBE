22de5b8be24d4892aff6bdc6eb74c075354ac666
ITCHYNS <izharfadillah19@gmail.com>
2025-11-22 17:40:56 +0700

    update homeview

--- SmartTelco-FrontEnd/src/views/HomeView.vue
+++ SmartTelco-FrontEnd/src/views/HomeView.vue
@@ -3,7 +3,7 @@
 
       <!-- HERO SECTION (Updated + Image Aside Right) -->
   <div
-    class="bg-gradient-to-br from-yellow-600 to-white-500 rounded-3xl p-10 shadow-2xl shadow-black/40 relative overflow-hidden max-w-10xl mx-auto">
+    class="bg-white-500 rounded-3xl shadow-2xl shadow-black/40 relative overflow-hidden max-w-10xl mx-auto">
 
     <!-- overlay glossy -->
     <div class="absolute top-0 left-0 w-full h-full bg-white/5 pointer-events-none"></div>
@@ -12,37 +12,46 @@
     <div class="relative z-10 flex flex-col md:flex-row items-center md:items-start">
 
       <!-- Kolom kiri: teks -->
-      <div class="text-left max-w-xl flex-1">
-        <h1 class="text-4xl md:text-5xl font-extrabold text-white leading-tight mb-6 tracking-wide">
-          Sistem Rekomendasi Paket Telco Cerdas
-        </h1>
+      <div class="relative z-10 w-full md:w-1/2 p-10 md:py-12 md:pr-10 md:pl-35 flex flex-col justify-center items-start">
+  
+  <h1 class="text-4xl md:text-5xl font-extrabold text-white leading-tight mb-6 tracking-wide">
+    Sistem Rekomendasi Paket Telco Cerdas
+  </h1>
 
-        <p class="text-blue-100 mb-10 leading-relaxed text-base md:text-lg">
-          Didukung oleh <strong>Machine Learning (XGBoost)</strong> untuk personalisasi penawaran paket yang akurat
-          berdasarkan pola perilaku pelanggan.
-        </p>
+  <p class="text-blue-100 mb-10 leading-relaxed text-base md:text-lg text-justify">
+    Didukung oleh <strong>Machine Learning (XGBoost)</strong> sistem ini melakukan personalisasi 
+    penawaran paket dengan akurasi tinggi. Kami menganalisis secara multidimensi pola perilaku 
+    pelanggan—mulai dari frekuensi top-up, rata-rata penggunaan data (GB), durasi panggilan (Menit), 
+    hingga skor keluhan (Complaint Count)—untuk memprediksi penawaran yang paling optimal. 
+    Hasilnya: peningkatan konversi penjualan dan loyalitas pelanggan karena setiap rekomendasi terasa sangat relevan.
+  </p>
 
-        <RouterLink
-          to="/simulation"
-          class="inline-block bg-[#FFC107] hover:bg-yellow-400 text-[#003366] font-extrabold py-4 px-8 rounded-xl shadow-lg transform hover:scale-105 transition duration-200 cursor-pointer">
-          MULAI SIMULASI & DAPATKAN REKOMENDASI
-        </RouterLink>
-      </div>
+  <RouterLink
+    to="/simulation"
+    class="inline-block bg-[#FFC107] hover:bg-yellow-400 text-[#842A3B] font-extrabold py-4 px-8 rounded-xl shadow-lg transform hover:scale-105 transition duration-200 cursor-pointer">
+    MULAI SIMULASI & DAPATKAN REKOMENDASI
+  </RouterLink>
+
+</div>
 
             <!-- Kolom kanan: gambar -->
-      <div class="mt-10 md:mt-0 md:pl-10 md:ml-auto flex justify-end">
-        <img
-          src="/images/SmartTelco_ilustration.jpg"
-          alt="Hero Illustration"
-          class="max-w-xs md:max-w-sm rounded-xl shadow-lg object-cover"
-        />
+      <div class="mt-10 md:mt-0 md:pl-0 md:ml-auto flex justify-end p-5">
+  
+          <div class="relative w-full max-w-md h-90 md:h-116 bg-white-500 rounded-3xl shadow-2xl overflow-hidden">
+            
+            <img
+              src="/images/SmartTelco_ilustration.jpg"
+              alt="Hero Illustration"
+              class="w-full h-full object-cover hover:scale-110 transition-transform duration-500"
+            />  
+        </div>
       </div>
     </div>
   </div>
 
     <!-- NILAI BISNIS -->
     <div>
-      <h2 class="text-center text-2xl font-bold mb-8 text-blue-200 drop-shadow-md">
+      <h2 class="text-center text-2xl font-bold mb-8 text-white drop-shadow-md">
         Nilai Bisnis & Inovasi
       </h2>
 
@@ -81,7 +90,7 @@
 
     <!-- CTA TERAKHIR -->
     <div
-      class="bg-gradient-to-br from-yellow-600 to-white-500 rounded-2xl p-10 text-center shadow-xl max-w-6xl mx-auto relative z-10">
+      class="bg-white-500 rounded-2xl p-10 text-center shadow-2xl shadow-black/40 max-w-6xl mx-auto relative z-10">
       <h3 class="text-xl md:text-2xl font-bold mb-6 text-white">
         Siap Menguji Kecerdasan Model Kami?
       </h3>
